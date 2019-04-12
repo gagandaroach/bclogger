190cb066ae732e1b0a7b36f3e3aad93907902277
gagany <gagandaroach@gmail.com>
2019-04-12 18:55:59 -0500

    add navigationx
    navigating to host fragment dashboard

--- app/src/main/java/tech/daroach/bclogger/MainActivity.kt
+++ app/src/main/java/tech/daroach/bclogger/MainActivity.kt
@@ -1,9 +1,20 @@
 package tech.daroach.bclogger
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import tech.daroach.bclogger.ui.Dashboard
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Dashboard.OnFragmentInteractionListener {
+
+    companion object {
+        const val TAG = "androidx_is_nice"
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+        Log.d(TAG, "OnFragmentInteraction: " + uri.toString()) //TODO: figure out what this is needed for.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
