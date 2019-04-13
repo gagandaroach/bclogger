9ecd7c906317a8cb348d1ba58f6bc6203d0525d3
gagany <gagandaroach@gmail.com>
2019-04-13 05:50:47 -0500

    bot nav bar code

--- app/src/main/java/tech/daroach/bclogger/MainActivity.kt
+++ app/src/main/java/tech/daroach/bclogger/MainActivity.kt
@@ -4,6 +4,10 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 import tech.daroach.bclogger.ui.Dashboard
 
 class MainActivity : AppCompatActivity(), Dashboard.OnFragmentInteractionListener {
@@ -13,11 +17,15 @@ class MainActivity : AppCompatActivity(), Dashboard.OnFragmentInteractionListene
     }
 
     override fun onFragmentInteraction(uri: Uri) {
-        Log.d(TAG, "OnFragmentInteraction: " + uri.toString()) //TODO: figure out what this is needed for.
+        Log.d(TAG, "OnFragmentInteraction: " + uri.toString()) //TODO: figure out what this is needed for. added for navigation x
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //bottom navigation link with navigation x
+        val navController = findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(findViewById<BottomNavigationView>(R.id.bottom_nav), navController);
     }
 }
